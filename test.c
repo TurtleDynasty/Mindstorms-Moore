7fb2c291e36b24d523d3453432de3272ad7c0bba
TurtleDynasty <TurtleDynasty@users.noreply.github.com>
2016-05-13 17:02:28 -0700

    Define test.c
    intermediate testing for developing moore.h, atomic, has nothing to do
    with future apps

---
+++ test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <moore.h>
+
+int main(){
+	struct MooreState q0;
+	struct MooreState q1;
+	struct MooreState q2;
+	struct MooreState q3;
+	
+	struct MooreTransition q0_a;
+	struct MooreTransition q0_b;
+	struct MooreTransition q1_a;
+	struct MooreTransition q1_b;
+	struct MooreTransition q2_a;
+	struct MooreTransition q2_b;
+	struct MooreTransition q3_a;
+	struct MooreTransition q3_b;
+
+	
+}
