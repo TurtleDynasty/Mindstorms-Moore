1019c83c0b081be2412fbf9ea2fb1affa6b80a15
TurtleDynasty <TurtleDynasty@users.noreply.github.com>
2016-05-13 19:44:48 -0700

    Essential object behavior is complete but untested
    (because it's time for me to go now), somewhat atomic.

--- moore.h
+++ moore.h
@@ -1,49 +1,91 @@
 #ifndef MOORE_H
 #define MOORE_H
 
-using namespace std;
+using namespace std; // using std:string and std:iostream
 
-class MooreTransition;
-
-
+class MooreMachine;
+class MooreState;
+class MooreTransition; // forward declarations
 
 class MooreState{
-public:
-	string name;
-	char output;
-	int num_transitions;
-	MooreTransition *transitions;
+	friend class MooreTransition;
+	friend class MooreMachine;
 
-	MoreState( string n, char o){
-		name = n;
-		output = o;
-	}
-	/*~MoreState(){
-		for (int i=0; i<num_transitions; i++){
-			transitions[i]
+	private:
+		string name;
+		char output;
+		int num_transitions;
+		MooreTransition *transitions;
+	
+		MoreState(){
+			num_transitions = 0;
 		}
-	} //Need to delete all pointers and stuff to transistions*/
 
-	void printMooreState(){
-		cout << name << " has " << num_transitions << " transitions: " <<endl;
-	}
+		MoreState( string n, char o){
+			name = n;
+			output = o;
+			num_transitions = 0;
+		}
+		/*~MoreState(){
+			for (int i=0; i<num_transitions; i++){
+				transitions[i]
+			}
+		} // Need to delete all pointers and stuff to transistions*/
+	
+		void printMooreState(){
+			cout << name << " has " << num_transitions << " transitions: " <<endl;
+		}
+
+	public:
+		void setName(string n){
+			this->name = n;
+		}
+		void setOutput(char o){
+			this->output = o;
+		}
 };
 
 class MooreTransition{
-public:
-	char input;
-	MooreState *destination;
+	friend class MooreState;
+	friend class MooreMachine;
 
-	void printMooreTransition(){
-		cout << "  " << input << " -> " << destination->name << endl;
-	}
+	private:
+		char input;
+		MooreState *destination;
+	
+		void printMooreTransition(){
+			cout << "  " << input << " -> " << destination->name << endl;
+		}
 };
 
-void printState(MooreState q){
-	q.printMooreState();
-	for (int i=0; i<q.num_transitions; i++) {
-			q.transitions[i].printMooreTransition();
-	}
-}
+class MooreMachine{
+	friend class MooreState;
+
+	private:
+		string name;
+		int num_states;
+		MooreState *states;
+
+	public:
+		MooreMachine(){
+			num_states = 0;
+		}
+
+		MooreMachine(string n){
+			name = n;
+			num_states = 0;
+		}
+
+		void printState(MooreState q){
+			q.printMooreState();
+			for (int i=0; i<q.num_transitions; i++) {
+					q.transitions[i].printMooreTransition();
+			}
+		}
+
+		void addState(MooreState){
+			//THIS is where you left off
+		}
+};
 
 #endif // MOORE_H
--- test.cpp
+++ test.cpp
@@ -2,30 +2,7 @@
 #include "moore.h"
 
 int main(){
-	MooreState q0;
-	MooreState q1;
-	MooreState q2;
-	MooreState q3;
+	// :)))))))))
 	
-	MooreTransition q0_a;
-	MooreTransition q0_b;
-	MooreTransition q1_a;
-	MooreTransition q1_b;
-	MooreTransition q2_a;
-	MooreTransition q2_b;
-	MooreTransition q3_a;
-	MooreTransition q3_b;
-
-	q0_a.input = 'a';
-	q0_a.destination = &q1;
-	q0_b.input = 'b';
-	q0_b.destination = &q0;
-	q0.name = "q0";
-	q0.output = '0';
-	q0.num_transitions = 2;
-	MooreTransition temp_array[q0.num_transitions] = {q0_a, q0_b}; 
-	q0.transitions = temp_array;
-
-	printState(q0);
 
 }
